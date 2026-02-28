ed26d54b393dee3d9e99c2854a8748bcb2b58592
vcnt <thiago.vicente@ua.pt>
2026-02-28 13:07:39 +0000

    Update

--- scripts/record-toggle.sh
+++ scripts/record-toggle.sh
@@ -2,14 +2,14 @@
 
 # Toggle screen recording and notify waybar
 
-if pgrep -x wf-recorder > /dev/null; then
-    # Stop recording
-    pkill -SIGINT wf-recorder
-    pkill -RTMIN+8 waybar
+if pgrep -x wf-recorder >/dev/null; then
+  # Stop recording
+  pkill -SIGINT wf-recorder
+  pkill -RTMIN+8 waybar
 else
-    # Start recording
-    FILENAME=~/Videos/recording_$(date +'%Y-%m-%d_%H-%M-%S').mp4
-    wf-recorder -g "$(slurp)" -f "$FILENAME" &
-    sleep 0.5
-    pkill -RTMIN+8 waybar
+  # Start recording
+  FILENAME=~/Videos/recording_$(date +'%Y-%m-%d_%H-%M-%S').mp4
+  wf-recorder --no-dmabuf -x yuv420p -g "$(slurp)" -f "$FILENAME" &
+  sleep 0.5
+  pkill -RTMIN+8 waybar
 fi
